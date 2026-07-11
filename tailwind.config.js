fc2b984bfdd50c7ab21d0a1c8d8094b17a564f19
ngendakuriyoleonce <ndendakuriyoleonce75@gmail.com>
2026-07-11 18:51:33 +0400

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                serif: ['Lora', ...defaultTheme.fontFamily.serif],
+            },
+            colors: {
+                brand: {
+                    50: '#FFF7ED',
+                    100: '#FFEDD5',
+                    200: '#FED7AA',
+                    300: '#FDBA74',
+                    400: '#FB923C',
+                    500: '#F97316',
+                    600: '#EA580C',
+                    700: '#C2410C',
+                    800: '#9A3412',
+                    900: '#7C2D12',
+                },
+            },
+        },
+    },
+
+    plugins: [forms],
+};
