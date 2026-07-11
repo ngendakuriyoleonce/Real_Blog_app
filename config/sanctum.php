f1a84ff5e341123afd54f20c16390e631da917d5
ngendakuriyoleonce <ndendakuriyoleonce75@gmail.com>
2026-07-11 18:52:39 +0400

    Add Sanctum API token configuration

---
+++ config/sanctum.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'driver' => env('SANCTUM_GUARD', ['web']),
+    'expiration' => null,
+    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
+    'middleware' => [
+        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
+        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+    ],
+];
